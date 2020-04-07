5b5e4350d1c66f62d4728b9ecef2365699a73713
Yongzhe Zhang <jcddyongzhe@gmail.com>
2020-04-07 20:51:40 +0800

    update some classes

--- src/entity/Payment.java
+++ src/entity/Payment.java
@@ -5,13 +5,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Payment {
+    private LocalDateTime checkInTime;
     private LocalDateTime checkOutTime;
     private double roomCharge;
     private double tax;
     private double roomServiceCharge;
     private double discount;
+    private double totalPay;
     private String paymentMethod;
-    private RoomService roomService;
+
+    public Payment(LocalDateTime checkInTime, LocalDateTime checkOutTime, double roomCharge, double tax,
+            double roomServiceCharge, double discount, String paymentMethod) {
+        this.checkInTime = checkInTime;
+        this.checkOutTime = checkOutTime;
+        this.roomCharge = roomCharge;
+        this.tax = tax;
+        this.roomServiceCharge = roomServiceCharge;
+        this.discount = discount;
+        this.paymentMethod = paymentMethod;
+        // not quite sure??
+        this.totalPay = (roomCharge + roomServiceCharge) *(1+ tax) * (1-discount);
+    }
+
+    public LocalDateTime getCheckInTime() {
+        return checkInTime;
+    }
 
     public LocalDateTime getCheckOutTime() {
         return checkOutTime;
@@ -21,33 +39,20 @@ public class Payment {
         return roomCharge;
     }
 
-    public void setRoomCharge(double roomCharge) {
-        this.roomCharge = roomCharge;
-    }
-
     public double getTax() {
         return tax;
-    }
-
-    public void setTax(double tax) {
-        this.tax = tax;
     }
 
     public double getRoomServiceCharge() {
         return roomServiceCharge;
     }
 
-    public void setRoomServiceCharge(double roomServiceCharge) {
-        this.roomServiceCharge = roomServiceCharge;
-    }
-
-
     public double getDiscount() {
         return discount;
     }
 
-    public void setDiscount(double discount) {
-        this.discount = discount;
+    public double getTotalPay() {
+        return totalPay;
     }
 
     public String getPaymentMethod() {
@@ -56,52 +61,5 @@ public class Payment {
 
     public void setPaymentMethod(String paymentMethod) {
         this.paymentMethod = paymentMethod;
-    }
-
-    public RoomService getRoomService() {
-        return roomService;
-    }
-
-    public void setRoomService(RoomService roomService) {
-        this.roomService = roomService;
-    }
-
-    //Calculate the room charge
-    public double calRoomCharge(Room room) {
-        double price=0;
-        double total_price=0;
-        LocalDateTime checkinTime = room.getGuest().getCheckIn();
-        List<Integer> days = getDays(checkinTime, checkOutTime);
-        for (int day : days) {
-            if (day == 0 || day == 6) {
-                price = room.getRoomRateWeekend();
-            }
-            price = room.getRoomRateWeekday();
-            total_price += price;
-
-        }
-        return total_price;
-    }
-
-    public static List<Integer> getDays(LocalDateTime checkin, LocalDateTime checkout){
-        List<Integer> days = new ArrayList<Integer>();
-        long duration = Duration.between(checkin,checkout).toDays();
-        int checkin_ = checkin.getDayOfWeek().getValue();
-
-        for(int i = 0; i< duration; i++){
-            days.add(checkin_);
-            checkin_=(checkin_+1)%7;
-        }
-        return days;
-    }
-
-    public double calRoomServiceCharge(RoomService roomService) {
-        double total_room_service = 0;
-        double item_price = 0;
-        for (Order orders : roomService.getOrders()){
-            item_price=orders.getMenuItem().getPrice();
-            total_room_service +=item_price;
-        }
-        return total_room_service;
     }
 }
