cf2f53008bd72c7e32ef2af6db4e25240d7cfb3f
Yongzhe Zhang <jcddyongzhe@gmail.com>
2020-04-20 12:23:39 +0800

    Create Boundary.java
    As the paraent class of boundaries

---
+++ src/boundary/Boundary.java
@@ -0,0 +1,15 @@
+package boundary;
+
+public class Boundary {
+    public void displayMain(){
+        System.out.println("Hotel Reservation and Payment System (HRPS)");
+		System.out.println("===========================================");
+		System.out.println("1. About Guest");
+		System.out.println("2. About Room");
+		System.out.println("3. About Reservation");
+		System.out.println("4. About Room Service");
+		System.out.println("5. About Payment");
+		System.out.println("6. Quit");
+		System.out.println("============================================");
+    }
+}
