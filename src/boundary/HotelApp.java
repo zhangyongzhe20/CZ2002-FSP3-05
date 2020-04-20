a02472966fe6e4d32bd1e1996830caf8a271bb48
Yongzhe Zhang <jcddyongzhe@gmail.com>
2020-04-20 12:23:46 +0800

    Update HotelApp.java

--- src/boundary/HotelApp.java
+++ src/boundary/HotelApp.java
@@ -1,39 +1,27 @@
-import java.util.ArrayList;
+package boundary;
 import java.util.Scanner;
-
 public class HotelApp {
 	public static void main(String[] args) {
-		 //Load data from text file
-		 ArrayList<Guest> guestList = new ArrayList<Guest>();
-
 		 Scanner sc = new Scanner(System.in);
 		 int choice;
-		 do {		
-			 printMenu();
-			 choice = sc.nextInt();
-	    
+		 do {
+			Boundary main = new Boundary();
+			main.displayMain();
+			choice = Integer.parseInt(sc.nextLine());
 			 switch(choice) {
-			 	case 1:
-			 		GuestManager.printGuestMenu(guestList);
-			 	case 2:
-			 	case 3:
-			 	case 4:
-			 	case 5:
-			 	case 6:
+				 case 1:
+				 break;
+				 case 2:
+				 break;
+				 case 3:
+				 break;
+				 case 4:
+				 main = new order_Boundary();
+				 main.displayMain();
+				 break;
+				 case 5:
+				 break;
 			 		}
-			 }while(choice !=6);
-	}
-	
-	
-	public static void printMenu() {
-		System.out.println("Hotel Reservation and Payment System (HRPS)");
-		System.out.println("===========================================");
-		System.out.println("1. About Guest");
-		System.out.println("2. About Room");
-		System.out.println("3. About Reservation");
-		System.out.println("4. About Room Service");
-		System.out.println("5. About Payment");
-		System.out.println("6. Quit");
-		System.out.println("============================================");
+			 }while(choice!=6);
 	}
 }
