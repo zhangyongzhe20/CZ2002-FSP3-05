be6073bd9397443a5d13c569990a0fcfbac4c951
Yongzhe Zhang <jcddyongzhe@gmail.com>
2020-04-21 20:14:41 +0800

    adjust boundary

--- src/boundary/Boundary.java
+++ src/boundary/Boundary.java
@@ -9,33 +9,25 @@ import java.util.List;
 import java.util.Scanner;
 
 
-public class Boundary {
-    Scanner sc = new Scanner(System.in);
-    int choice;
 
+public class Boundary {
     Reservation_Boundary reservationpage;
     Guest_Boundary guestpage;
 
 
-    public void displayMain(){
+    public void display_Control(){
+        int choice;
+        Scanner sc = new Scanner(System.in);
         Order_Boundary orderpage = new Order_Boundary();
         Room_Boundary roompage= new Room_Boundary();
         orderpage.loadData();
         roompage.loadData();
-    //    roompage.loadData;
+            //    roompage.loadData;
     //    reservationpage.loadData;
     //    guestpage.loadData;
-        do {
-        System.out.println("Hotel Reservation and Payment System (HRPS)");
-		System.out.println("===========================================");
-		System.out.println("1. About Guest");
-		System.out.println("2. About Room");
-		System.out.println("3. About Reservation");
-		System.out.println("4. About Room Service");
-		System.out.println("5. About Payment");
-		System.out.println("6. Quit");
-        System.out.println("============================================");
-		choice = Integer.parseInt(sc.nextLine());
+    do{
+        displayMain();
+        choice = Integer.parseInt(sc.nextLine());
 		switch(choice) {
 			case 1:
 			break;
@@ -46,19 +38,34 @@ public class Boundary {
         	 case 4:
              orderpage.displayMain();
 			 break;
-			 case 5:
-			 break;
+             case 5:
+             break;
+             case 6:
 			}
-			 }while(choice!=6);
-		List<String> data = new ArrayList<String>();
-		HashMap<String , String> enumData = new HashMap<String , String>();
-		enumData.put("1", "WALKIN" );
-		enumData.put("2", "RESERVATION");
-		readInputEnum("1. WALKIN\n 2.RESERVATION\n" , enumData);
-		System.out.println("============================================");
+			 }while(choice!=7);
+		// List<String> data = new ArrayList<String>();
+		// HashMap<String , String> enumData = new HashMap<String , String>();
+		// enumData.put("1", "WALKIN" );
+		// enumData.put("2", "RESERVATION");
+		// readInputEnum("1. WALKIN\n 2.RESERVATION\n" , enumData);
+		// System.out.println("============================================");
+    }
+
+
+    public void displayMain(){
+        System.out.println("Hotel Reservation and Payment System (HRPS)");
+		System.out.println("===========================================");
+		System.out.println("1. About Guest");
+		System.out.println("2. About Room");
+		System.out.println("3. About Reservation");
+        System.out.println("4. About Room Service");
+        System.out.println("5. About Checkin");
+        System.out.println("6. About Checkout");
+		System.out.println("7. Quit");
+        System.out.println("============================================");
     }
     
-   // public abstract void loadData();
+    public void loadData(){};
 
     public String readInputString(String message) {
     	System.out.println(message);
